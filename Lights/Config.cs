707e6757eadbbd204fb2120463a441f7dc5aee68
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-02-28 00:54:40 +0500

    Add files via upload

---
+++ Lights/Config.cs
@@ -0,0 +1,93 @@
+﻿using Qurre.API.Addons;
+using Qurre.API.Controllers;
+using Qurre.API.Objects;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Lights
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "Lights";
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("You can customize lights: room/color/pos etc?")]
+        public List<MakeLight> MakingLights { get; set; } = new List<MakeLight>()
+        {
+            new MakeLight
+            {
+                Light_Name = "Tutorial Room",
+                Room = RoomType.Surface,
+                Color = "yellow",
+                Position = new Vector3(53.5f, 20f, -44.2f),
+                Intensive = 0.8f,
+                Range = 10f
+            },
+            new MakeLight
+            {
+                Light_Name = "SCP-106 Room",
+                Room = RoomType.Pocket,
+                Color = "yellow",
+                Position = new Vector3(0, 4, 0),
+                Intensive = 1f,
+                Range = 40f
+            },
+            new MakeLight
+            {
+                Light_Name = "Surface Nuke",
+                Room = RoomType.Surface,
+                Color = "yellow",
+                Position = new Vector3(41, -9, -37),
+                Intensive = 1.5f,
+                Range = 10f
+            },
+            new MakeLight
+            {
+                Light_Name = "Chaos Spawn",
+                Room = RoomType.Surface,
+                Color = "yellow",
+                Position = new Vector3(0, -5, -58),
+                Intensive = 1f,
+                Range = 20f
+            },
+            new MakeLight
+            {
+                Light_Name = "MTF Elevator",
+                Room = RoomType.Surface,
+                Color = "yellow",
+                Position = new Vector3(87.7f, -3.5f, -48.5f),
+                Intensive = 1f,
+                Range = 10f
+            },
+            new MakeLight
+            {
+                Light_Name = "Alpha-Warhead",
+                Room = RoomType.HczNuke,
+                Color = "yellow",
+                Position = new Vector3(-3.6f, 403.3f, 14.8f),
+                Intensive = 1f,
+                Range = 10f
+            },
+            new MakeLight
+            {
+                Light_Name = "SCP-096 Room",
+                Room = RoomType.Hcz096,
+                Color = "yellow",
+                Position = new Vector3(0, 3, 0),
+                Intensive = 1f,
+                Range = 10f
+            },
+        };
+    }
+    public class MakeLight
+    {
+        public string Light_Name { get; set; }
+        public RoomType Room { get; set; }
+        public string Color { get; set; }
+        public Vector3 Position { get; set; }
+        public float Intensive { get; set; }
+        public float Range { get; set; }
+    }
+}
---
+++ Lights/Lights.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Qurre;
+using Qurre.API;
+using Qurre.Events;
+using Qurre.API.Events;
+using Qurre.API.Objects;
+using UnityEngine;
+using Mirror;
+using MEC;
+
+using Version = System.Version;
+using Player = Qurre.API.Player;
+
+namespace Lights
+{
+    public class Lights : Plugin
+    {
+        public override string Developer => "KoT0XleB#4663";
+        public override string Name => "Lights";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public static Config CustomConfig { get; set; }
+
+        public static GameObject LightsObject;
+        public static List<GameObject> gameObjects = new List<GameObject>();
+
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start += OnRoundStarted;
+            Qurre.Events.Round.End += OnRoundEnded;
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start -= OnRoundStarted;
+            Qurre.Events.Round.End -= OnRoundEnded;
+        }
+        public void OnRoundStarted()
+        {
+            Timing.CallDelayed(10f, () =>
+            {
+                foreach(Player player in Player.List)
+                {
+                    player.TeleportToRoom(RoomType.Surface);
+                    Log.Info(player.Position);
+                }
+            });
+            foreach(var list in CustomConfig.MakingLights)
+            {
+                Color needColor;
+                switch (list.Color)
+                {
+                    case "yellow":
+                        needColor = Color.yellow;
+                        break;
+                    case "white":
+                        needColor = Color.white;
+                        break;
+                    case "black":
+                        needColor = Color.black;
+                        break;
+                    case "gray":
+                        needColor = Color.gray;
+                        break;
+                    case "red":
+                        needColor = Color.red;
+                        break;
+                    case "green":
+                        needColor = Color.green;
+                        break;
+                    case "cyan":
+                        needColor = Color.cyan;
+                        break;
+                    default:
+                        needColor = Color.gray;
+                        break;
+                }
+                CreateLight(list.Room, needColor, list.Position, list.Intensive, list.Range);
+            }
+        }
+        public void CreateLight(RoomType room, Color color, Vector3 localposition, float Intensive, float Range)
+        {
+            var Light = new Qurre.API.Controllers.Light(localposition, color, Intensive, Range);
+            LightsObject = Light.Base.gameObject;
+            NetworkServer.UnSpawn(LightsObject);
+            LightsObject.transform.parent = room.GetRoom().Transform;
+            LightsObject.transform.localPosition = localposition;
+            NetworkServer.Spawn(LightsObject);
+            gameObjects.Add(LightsObject);
+        }
+        public void OnRoundEnded(RoundEndEvent ev)
+        {
+            foreach (GameObject gameObject in gameObjects)
+            {
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+}
